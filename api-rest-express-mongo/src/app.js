7454f3ebc75cdfc9ca3e8370b582145eb80a085e
igorhgds <igorhenrique.gomes@hotmail.com>
2025-10-14 18:21:34 -0300

    Express e primeiras rotas

--- api-rest-express-mongo/src/app.js
+++ api-rest-express-mongo/src/app.js
@@ -9,6 +9,12 @@ const livros = [
     { id: 3, titulo: "O Hobbit" }
 ]
 
+function buscaLivro(id){
+    return livros.findIndex(livro => {
+        return livro.id === Number(id);
+    });
+}
+
 app.get("/", (req, res) => {
     res.status(200).send("Curso de Node.js");
 });
@@ -17,9 +23,26 @@ app.get("/livros", (req, res) => {
     res.status(200).json(livros);
 });
 
+app.get("/livros/:id", (req, res) => {
+    const index = buscaLivro(req.params.id);
+    res.status(200).json(livros[index]);
+})
+
 app.post("/livros", (req, res) => {
     livros.push(req.body);
     res.status(201).send("Livro adicionado com sucesso");
+});
+
+app.put("/livros/:id", (req, res) => {
+    const index = buscaLivro(req.params.id);
+    livros[index].titulo = req.body.titulo;
+    res.status(200).send("Livro atualizado com sucesso");
+});
+
+app.delete("/livros/:id", (req, res) => {
+    const index = buscaLivro(req.params.id);
+    livros.splice(index, 1);
+    res.status(204);  
 });
 
 export default app;
